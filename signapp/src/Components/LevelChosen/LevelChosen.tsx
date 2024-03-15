764d195d82936f2083a6de7477d1d6dd58e3a329
0VINCI <101973216+0VINCI@users.noreply.github.com>
2024-03-16 00:35:13 +0100

    btn

--- signapp/src/Components/LevelChosen/LevelChosen.tsx
+++ signapp/src/Components/LevelChosen/LevelChosen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button'; 
-import '../App/App.css'; // Upewnij się, że ścieżka do App.css jest poprawna
+import './LevelChosen.css';
 
 const LevelChosen = () => {
   const navigate = useNavigate();
